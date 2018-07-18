2322aa57a47a68d4062683159ed5519f9b58b254
chens <3176589556@qq.com>
2018-07-19 00:30:49 +0800

    增加控制器

---
+++ ios/Demo01/Demo01/TwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  TwoViewController.h
+//  Demo01
+//
+//  Created by mac on 2018/7/18.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwoViewController : UIViewController
+
+@end
---
+++ ios/Demo01/Demo01/TwoViewController.m
@@ -0,0 +1,67 @@
+//
+//  TwoViewController.m
+//  Demo01
+//
+//  Created by mac on 2018/7/18.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import "TwoViewController.h"
+#import <AFNetworking.h>
+
+@interface TwoViewController ()
+
+@end
+
+@implementation TwoViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://example.com/download.zip"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response)
+    {
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+        
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error)
+    {
+        NSLog(@"File downloaded to: %@", filePath);
+    }];
+    
+    [downloadTask resume];
+    
+    
+    
+}
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
